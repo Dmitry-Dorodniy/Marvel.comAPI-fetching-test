ccefe106f19d3889c80756e4d26ac76f6d35a39c
DmitryDorodniy <dmitrydorodniy@gmail.com>
2022-08-11 16:04:28 +0300

    cache images in tableView

--- iOS6-HW25-DmitryDorodniy/Sources/Extentions/UIImageView + Ext.swift
+++ iOS6-HW25-DmitryDorodniy/Sources/Extentions/UIImageView + Ext.swift
@@ -1,17 +1,23 @@
 import UIKit
 
+var imageCahce = NSCache<AnyObject, AnyObject>()
 extension UIImageView {
     func loadImage(at urlString: String) {
 
+        if let image = imageCahce.object(forKey: urlString as NSString) as? UIImage {
+            self.image = image
+            return
+        }
         guard let url = URL(string: urlString) else { return }
         DispatchQueue.global().async { [weak self] in
             if let data = try? Data(contentsOf: url) {
                 if let image = UIImage(data: data) {
                     DispatchQueue.main.async {
+                        imageCahce.setObject(image, forKey: urlString as NSString)
                         self?.image = image
                     }
                 }
-        }
+            }
         }
     }
 }
